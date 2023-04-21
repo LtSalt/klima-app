98d017dfa5ac55f4eb83a2951c804af224110c4d
David Will <64697174+LtSalt@users.noreply.github.com>
2023-04-21 19:03:39 +0200

    prisma db setup & surface

---
+++ src/routes/+page.server.js
@@ -0,0 +1,25 @@
+import { prisma } from "$lib/server/prisma.js"
+
+export async function load() {
+    const getGeometries = async() => {
+        const data =  await prisma.geometry.findMany()
+        return Object.fromEntries(data.map(d => [d.level, d.geojson]))
+        }
+    const getDistricts = async() => {
+        return await prisma.district.findMany()
+    }
+    const getMetrics = async() => {
+        return await prisma.Periodic_Annual.findMany({
+            where: {
+                period: "2018-2022"
+            },
+            select: {
+                ars: true,
+                period: true,
+                temp: true,
+                temp_dev: true
+            }
+        })
+    }
+    return { geo: getGeometries(), pool: getDistricts(), metrics: getMetrics()}
+}
